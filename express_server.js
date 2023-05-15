1f65932d8385a100eafa87d7bf5e00f38ec0e2f7
Clayton <persinger.clayton@gmail.com>
2023-05-15 12:23:19 -0700

    Install ejs. Add ejs as view engine in main server file.

--- express_server.js
+++ express_server.js
@@ -2,10 +2,14 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -17,7 +21,7 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port: ${PORT}`);
