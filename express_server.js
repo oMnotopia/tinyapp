76e160056347b7acae1b6cb401f6d7cf25f01e8a
Clayton <persinger.clayton@gmail.com>
2023-05-15 19:02:42 -0700

    Update POST route to generate random string and assign as key and the posted longURL as a key value pair to the database object. Create new GET route where the user can enter /u/ and the shortened URL and it will redirect to the associated longURLs website.

--- express_server.js
+++ express_server.js
@@ -2,17 +2,15 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
-const alphaNumericArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const alphaNumericArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 const generateRandomString = () => {
   let randStr = '';
   while (randStr.length < 6) {
-    const randNum = Math.floor(Math.random() * 36);
+    const randNum = Math.floor(Math.random() * 62);
     randStr += alphaNumericArr[randNum];
   }
-  return String(randStr);
+  return randStr;
 };
-
-console.log(generateRandomString());
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -27,14 +25,21 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/u/:id", (req, res) => {
+  console.log(req.params.id);
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const key = generateRandomString();
+  urlDatabase[key] = req.body.longURL;
+  res.redirect(`/urls/${key}`);
 });
 
 app.get("/urls/new", (req, res) => {
