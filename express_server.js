4331edeb4c379ff5aa1b4d1de6b94ff12893972a
Clayton <persinger.clayton@gmail.com>
2023-05-16 12:55:23 -0700

    Create new POST method that updates the database's longURL based on the shortURL.

--- express_server.js
+++ express_server.js
@@ -48,6 +48,11 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls/:id", (req, res) => {
+  urlDatabase[req.params.id] = req.body.longURLEdit;
+  res.redirect("/urls");
+});
+
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect("/urls");
@@ -60,10 +65,6 @@ app.get("/u/:id", (req, res) => {
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
