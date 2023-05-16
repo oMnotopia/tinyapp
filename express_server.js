dd52ac080c8ab1cb1d15a9dea2c5161cb2d6f6a5
Clayton <persinger.clayton@gmail.com>
2023-05-15 17:18:48 -0700

    Create view for adding a new URL to the database. Add GET and POST methods to the /URL path so that when a user submits the form on the page the values can be grabbed and used.

--- express_server.js
+++ express_server.js
@@ -2,6 +2,20 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+const alphaNumericArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const generateRandomString = () => {
+  let randStr = '';
+  while (randStr.length < 6) {
+    const randNum = Math.floor(Math.random() * 36);
+    randStr += alphaNumericArr[randNum];
+  }
+  return String(randStr);
+};
+
+console.log(generateRandomString());
+
+app.use(express.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -16,6 +30,15 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
