1105448842ff03acf2f2f3daddb2e9a0cf5d7f8d
Clayton <persinger.clayton@gmail.com>
2023-05-17 14:20:19 -0700

    Update POST route for /logout to redirect to /login page.

--- express_server.js
+++ express_server.js
@@ -89,7 +89,7 @@ app.post("/login", (req, res) => {
 
 app.post("/logout", (req, res) => {
   res.clearCookie("user_id");
-  res.redirect("/urls");
+  res.redirect("/login");
 });
 
 // ----------------- Register ----------------- //
