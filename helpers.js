6c7ac5a43ce7b673a938abe602762d6612983504
Clayton <persinger.clayton@gmail.com>
2023-05-18 15:17:00 -0700

    Create function that returns the current date written as month day, year.

--- helpers.js
+++ helpers.js
@@ -14,6 +14,15 @@ const urlsForUser = (id, urlDatabase) => {
   return filteredObj;
 };
 
+const getCurrentDate = () => {
+  const monthNames = [ "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December" ];
+
+  const newDate = new Date();
+  const formattedDate = `${monthNames[newDate.getMonth()]} ${newDate.getDate()}, ${newDate.getFullYear()}`;
+  return formattedDate;
+};
+
 const generateRandomString = () => {
   const alphaNumericArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   let randStr = '';
@@ -24,4 +33,4 @@ const generateRandomString = () => {
   return randStr;
 };
 
-module.exports = { getUserByEmail, generateRandomString, urlsForUser };
+module.exports = { getUserByEmail, urlsForUser, getCurrentDate, generateRandomString };
